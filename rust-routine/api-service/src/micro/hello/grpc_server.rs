3adc4bcc48670f6bc2406b496652ddc6c7265079
axemc <192119741@qq.com>
2023-05-23 23:32:27 +0800

    update volo version

--- rust-routine/api-service/src/micro/hello/grpc_server.rs
+++ rust-routine/api-service/src/micro/hello/grpc_server.rs
@@ -1,4 +1,4 @@
-#![feature(type_alias_impl_trait)]
+#![feature(impl_trait_in_assoc_type)]
 
 use std::net::SocketAddr;
 
@@ -19,18 +19,6 @@ impl volo_gen::hello::Greeter for S {
         Ok(volo_grpc::Response::new(resp))
     }
 }
-// impl volo_gen::proto_gen::hello::Greeter for S {
-//     async fn say_hello(
-//         &self,
-//         req: volo_grpc::Request<volo_gen::proto_gen::hello::HelloRequest>,
-//     ) -> Result<volo_grpc::Response<volo_gen::proto_gen::hello::HelloReply>, volo_grpc::Status>
-//     {
-//         let resp = volo_gen::proto_gen::hello::HelloReply {
-//             message: format!("Hello, {}!", req.get_ref().name).into(),
-//         };
-//         Ok(volo_grpc::Response::new(resp))
-//     }
-// }
 
 #[volo::main]
 async fn main() {
